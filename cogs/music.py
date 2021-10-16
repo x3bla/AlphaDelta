92ba03d8df8f10157c630ec0f54f0509fe795f84
x3bla <75881405+x3bla@users.noreply.github.com>
2021-10-16 13:50:05 +0800

    Revert "Revert "save""
    This reverts commit 269b92f92e41b0bf64daeadeba7202f41f24208f.

--- cogs/music.py
+++ cogs/music.py
@@ -1,14 +1,15 @@
 import discord
 from discord.ext import commands
-from discord.voice_client import VoiceClient
+# from discord.voice_client import VoiceClient
 import youtube_dl
-import asyncio
+# import asyncio
+import os
 
-youtube_dl.utils.bug_reports_message = lambda: ''
+#youtube_dl.utils.bug_reports_message = lambda: ''  # ????
 
 # https://github.com/ytdl-org/youtube-dl/blob/master/README.md#post-processing-options
 ytdl_format_options = {  # sets the quality of the audio
-    'format': '249/250/251',
+    'format': '249/250/251',  # basically 'worstaudio' but youtube comment said to use this
     'outtmpl': 'zz-%(id)s-%(title)s.%(ext)s',  # z to put it at the most bottom
     # 'restrictfilenames': True,
     # 'noplaylist': True,
@@ -27,11 +28,14 @@ ffmpeg_options = {  # idk just don't delete
     'options': '-vn'
 }
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)  # youtube download
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)  # loading youtube download options
 
 
 # this class is all copy paste from https://github.com/RK-Coding/Videos/blob/master/rkcodingmusicqueue.py
-class YTDLSource(discord.PCMVolumeTransformer):
+class YTDLSource(discord.PCMVolumeTransformer):  # altering this to get info
+    title = ""
+    url = ""
+
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
 
@@ -40,17 +44,17 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.title = data.get('title')
         self.url = data.get('url')
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+#     @classmethod
+#     async def from_url(cls, url, *, loop=None, stream=False):
+#         loop = loop or asyncio.get_event_loop()
+#         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+#
+#         if 'entries' in data:
+#             # take first item from a playlist
+#             data = data['entries'][0]
+#
+#         filename = data['url'] if stream else ytdl.prepare_filename(data)
+#         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 client = commands.Bot(command_prefix='!')
@@ -98,25 +102,42 @@ class Music(commands.Cog):
             pass
 
         queue.append(title)
-        server = ctx.message.guild
-        voice_channel = server.voice_client
+        server = ctx.message.guild  # the server where the command is sent
+        voice_channel = server.voice_client  # the voice channel of the user who sent the command
 
         try:
             voice_channel.stop()  # ends the current song if there is
         except voice_channel.ClientException:
             pass
+        # add later
+        # song_there = os.path.isfile("song.webm")
+        # try:
+        #     if song_there:
+        #         os.remove("song.webm")
+
         try:
             async with ctx.typing():
-                player = await YTDLSource.from_url(queue[0], loop=client.loop)  # idk wtf is going on here
-                voice_channel.play(player, after=lambda e: print("Player error: %s" % e) if e else None)
+                # player = await YTDLSource.from_url(queue[0], loop=client.loop)  # idk wtf is going on here
+                # voice_channel.play(player, after=lambda e: print("Player error: %s" % e) if e else None)
+
+                with youtube_dl.YoutubeDL(ytdl_format_options) as ydl:
+                    ydl.download([title])
+                queue.append(YTDLSource.title)
+
+                for file in os.listdir("./"):
+                    if file.endswith(".webm"):
+                        os.rename(file, "song.webm")
+                voice_channel.play(discord.FFmpegOpusAudio("song.webm"))
 
                 if loop:
                     queue.append(queue[0])  # adding back into the queue if loop is True
 
                 del (queue[0])
-                await ctx.send(f"**Now playing:** {player.title}")
+                await ctx.send(f"**Now playing:** {playerTitle}")
         except discord.ClientException:  # except: already playing a song
             await ctx.send(f"`{title}` added to queue")
+
+            # reserved for autoplay...?
 
     @commands.command(aliases=["stop"])
     async def pause(self, ctx):
@@ -141,8 +162,8 @@ class Music(commands.Cog):
             voice_channel.stop()
 
             async with ctx.typing():
-                player = await YTDLSource.from_url(queue[0], loop=client.loop)  # idk wtf is going on here
-                voice_channel.play(player, after=lambda e: print("Player error: %s" % e) if e else None)
+                # player = await YTDLSource.from_url(queue[0], loop=client.loop)  # idk wtf is going on here
+                # voice_channel.play(player, after=lambda e: print("Player error: %s" % e) if e else None)
 
                 if loop:
                     queue.append(queue[0])  # adding back into the queue if loop is True
