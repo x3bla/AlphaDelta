89501950a1189e6d98333117d6cc7738db57045e
x3bla <75881405+x3bla@users.noreply.github.com>
2021-10-29 09:03:24 +0800

    server queue now works, checkpoint :D

--- cogs/music.py
+++ cogs/music.py
@@ -12,18 +12,19 @@ ytdlp_format_options = {  # sets the quality of the audio
     'format': 'worstaudio',
     'outtmpl': 'zz-%(id)s-%(title)s.%(ext)s',  # z to put it at the most bottom
     # 'restrictfilenames': True,
-    'noplaylist': True,
+    'noplaylist': False,
     # 'concurrent_fragment_downloads': 5,
     # 'keepvideo': False,
-    'max_filesize': 5000000,  # in Bytes
+    'max_filesize': 5000000,  # 5MB bytes
     # 'nocheckcertificate': True,
     # 'ignoreerrors': False,
     # 'logtostderr': False,
-    # 'quiet': False,
-    # 'no_warnings': True,
+    'quiet': True,
+    'no_warnings': True,
     'default_search': 'auto',
     # 'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }  # spam of comments to see which is unneeded
+
 
 ffmpeg_options = {  # idk just don't delete this
     'options': '-vn'
@@ -50,18 +51,15 @@ class VideoData:  # getting video's youtube title
 class VideoQueueItem:  # download the video
     def __init__(self, videoData: VideoData):
         self.videoData = videoData
-        self.randomList = [""]
 
     async def __download(self, fileName=None):
-        print(self.videoData.url)
-        self.randomList[0] = self.videoData.url
-        ytdl.download(self.randomList)
-        print("am done, shouldn't be fucking downloading any shit")
+        parseToList = [self.videoData.url]  # convert to ["song_name"]
+        ytdl.download(parseToList)  # they use a for loop, so the string breaks down into chars, どうしてわからない
 
     async def download(self, fileName=None):
-        await self.__download(self.videoData.url)
+        await self.__download()
 
-    def returnTitle(self):
+    def returnTitle(self):  #
         return self.videoData.title
 
     def printThis(self):
@@ -75,23 +73,21 @@ class VideoQueue:  # a queue for each server
     def displayQueue(self, server):
         return self.queue[server]
 
-    def addVideo(self, server, videoItem: VideoQueueItem):
-        videoTitle = videoItem.returnTitle()  # getting the video's title
-        if server not in self.queue:
-            self.queue[server] = videoTitle  # if the key "server's name" does not exist, create it
+    def addVideo(self, server, videoTitle):
+        if server not in self.queue:  # if the key "server's name" does not exist, create it.
+            self.queue[server] = [videoTitle]
         else:
             self.queue[server].append(videoTitle)
+        print(self.queue, "\n")
 
-    async def removeVideo(self, videoItem: int):
-        # del(self.queue[videoItem])
-        # print(self.queue[videoItem])
-        pass
+    async def removeVideo(self, server, videoItem: int):
+        del(self.queue[server][videoItem])
 
-    async def __searchVideo(self, server, searchString: str):
+    async def __searchVideo(self, server, searchString: str):  #
         data = await getVideoData(searchString)
         self.addVideo(server, VideoQueueItem(VideoData(data)))
 
-    async def searchAndAddVideo(self, server, searchString: str):
+    async def searchAndAddVideo(self, server, searchString: str):  #
         # loop = asyncio.get_event_loop()
         await self.__searchVideo(server, searchString)
 
@@ -102,34 +98,11 @@ class AutoPlay:
     async def play(self):
         raise NotImplementedError
 
-# this class is all copy paste from https://github.com/RK-Coding/Videos/blob/master/rkcodingmusicqueue.py
-# class YTDLSource(discord.PCMVolumeTransformer):  # altering this to get info
-#     def __init__(self, source, *, data, volume=0.5):
-#         super().__init__(source, volume)
-#
-#         self.data = data
-#
-#         self.title = data.get('title')
-#         self.url = data.get('url')
-#
-#     @classmethod
-#     async def from_url(cls, url, *, loop=None, stream=True):
-#         loop = loop or asyncio.get_event_loop()
-#         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-#
-#         if 'entries' in data:
-#             # take first item from a playlist
-#             data = data['entries'][0]
-#
-#         filename = data['url'] if stream else ytdl.prepare_filename(data)
-#         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
 
 client = commands.Bot(command_prefix='!')
 loop = False
 
 
-# this is one hell of a library
 class Music(commands.Cog):
 
     def __init__(self, bot):
@@ -173,7 +146,8 @@ class Music(commands.Cog):
 
         server = ctx.message.guild  # the server where the command is sent
         voice_channel = server.voice_client  # the voice channel of the user who sent the command
-        await queue.searchAndAddVideo(server, title)
+        print(server)
+        queue.addVideo(server, title)
 
         try:
             voice_channel.stop()  # ends the current song if there is
@@ -181,15 +155,15 @@ class Music(commands.Cog):
             pass
 
         async with ctx.typing():
-            temp = VideoQueueItem(VideoData(data))  # first item in search
+            temp = VideoQueueItem(VideoData(data))  # downloading, classes are troublesome
             await temp.download()
 
-            voice_channel.play(discord.FFmpegOpusAudio(ytdl.prepare_filename(data)))
+            voice_channel.play(discord.FFmpegOpusAudio(ytdl.prepare_filename(data)))  # play song
 
             if loop:
-                await queue.searchAndAddVideo(server, title)  # adding back into the queue if loop is True
-
-            await queue.removeVideo(0)  # removing first video from list since it's playing
+                queue.addVideo(server, current_song)  # adding back into the queue if loop is True
+            # TODO: MOVE DEL LINE TO AUTOPLAY
+            await queue.removeVideo(server, 0)  # removing first video from list since it's playing
             await ctx.send(f"**Now playing:** {current_song}")
 
             #
